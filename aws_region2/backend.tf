42fdfc5a0ef6efc6526fff38967569b9763597af
Som Shanker Pandey <117086505+Sompandey01@users.noreply.github.com>
2025-02-20 13:56:31 +0530

    Update backend.tf

--- aws_region2/backend.tf
+++ aws_region2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-    bucket         = "prod-tfstate-region2" # change this
+    bucket         = "prod-tfstate-region-2" # change this
     key            = "deepak/terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
