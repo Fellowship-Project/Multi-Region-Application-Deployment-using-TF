e420a8cf25c1f8a4ca4e4cdf17f9e93ff2e44e3e
Som Shanker Pandey <117086505+Sompandey01@users.noreply.github.com>
2025-03-03 14:53:53 +0530

    Enabled state locking

--- aws_region2/backend.tf
+++ aws_region2/backend.tf
@@ -1,8 +1,9 @@
 terraform {
    backend "s3" {
-    bucket         = "prod-tfstate-region-2" # change this
-    key            = "deepak/terraform.tfstate"
-    region         = "ap-southeast-1"
-    encrypt        = true
+      bucket         = "prod-tfstate-region-2" # change this
+      key            = "deepak/terraform.tfstate"
+      region         = "ap-southeast-1"
+      encrypt        = true
+      dynamodb_table = "terraform-lock-region-2"  # Enables locking
+   }
 }
- }
